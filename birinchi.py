f7221b7d7b04d2852078f6396c01a3337c2f300d
Behruz <behruzyunusboyev68@gmail.com>
2026-05-20 00:04:21 +0500

    1 , 2 - darslar tugadi

---
+++ birinchi.py
@@ -0,0 +1,129 @@
+# 1- DARS 18.05.2026 
+
+
+# print("salom dunyo")
+# print('mening ismim behruz')
+# print("men python organmoqdaman")
+# print("bu ishlaydi")
+
+# ism = input('ism kiriting :')
+# print('salom', ism)
+
+# print("salom", 'ali', 'aka')
+
+# print("2024", "01" , "15", sep="-")
+
+# print('salom', end=" ")
+# print('dunyo')
+
+# ism= "kamola"
+# yosh= '22'
+# print(f"mening ismmim {ism}, yoshim {yosh}")
+
+# print(5 + 3)
+# print(25 - 36)
+# print(8/4)
+# print(3 * 7)
+# print(15 // 4)
+# print(10 % 3)
+# print(2 ** 8)
+
+# ism = input('ism : ')
+# yosh= int(input('yoshingizni kiriting : '))
+# print(f'salom {ism}, 10 yildan song {yosh + 10} yoshda bolasiz')
+
+# 2- DARS 
+
+# ism= input('ismingizni kiriting : ')
+# yosh= int(input('yoshingizni kiriting : '))
+# print(f"{ism} siz hozir {yosh} yoshdasiz 10 yildan keyin {yosh +10 } da bolasiz")
+
+# ism= 'ali'
+# mening_ismim = 'behruz'
+# yoshim = 20
+
+# yosh =25
+# narx = 150000
+# temperatura = -5
+
+# print(type(yosh))
+# print(type(narx))
+# print(type(temperatura))
+
+# balandlik = 1.75
+# ogirlik= 2.13
+# foiz= 12.5
+
+# print(type(balandlik))
+
+# ism= 'ali'
+# shaxar= 'toshkent' 
+# tavsif= """salom bu
+# kopqatorli matn"""
+
+# print(type(ism))
+# print(tavsif)
+
+# 19.05.2026
+
+# yosh = int('20')
+# narx = float('15.5')
+# son = str(100)
+
+# print(f"{yosh}, {narx}, {son}")
+
+# ism = "ali toshmatov"
+
+# print(ism.upper())
+# print(ism.capitalize())
+# print(ism.title())
+# print(len(ism))
+# print(ism.replace('ali', 'vali'))
+
+# ism= "ali"
+# familya = "Karimov"
+
+# toliq = ism + ' ' + familya
+
+# print(toliq)
+# print(f"{ism}, {familya}")
+
+# x = y = z = 0
+
+# a, b, c = 1, 2, 3
+
+# a, b = b, a
+# print(a, b, c)
+# print(x, y, z)
+
+# hisob = 1000
+# print(hisob)
+
+# hisob = hisob + 500
+# print(hisob)
+
+# hisob += 200
+# print(hisob)
+
+# hisob -= 300
+# print(hisob)
+
+# hisob *= 2
+# print(hisob)
+
+# hisob //= 4
+# print(hisob)
+
+# ism= input('ismingiz ')
+# yosh = int(input('yoshingiz (sonda yozing) '))
+# boy = int(input('boyingiz '))
+# vazn= int(input('vazningiz '))
+# yosh100 = 100 - yosh
+# boy_metr= boy / 10
+# bmi= round(vazn / (boy_metr **2), 1)
+
+
+# print(f'salom {ism} siz 100 yosh bolishingiz gacha {yosh100} yil bor bmi ingiz {bmi}')
+
+# 2-DARS TUGADI
+

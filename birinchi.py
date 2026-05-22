ef313b94f9c5f4459f3fe66288321d348e1a486a
Behruz <behruzyunusboyev68@gmail.com>
2026-05-22 23:05:04 +0500

    3- dars va topshiriq tugadi if va else organildi

--- birinchi.py
+++ birinchi.py
@@ -127,3 +127,96 @@
 
 # 2-DARS TUGADI
 
+# 3-DARS START
+
+# yosh = int(input('yoshingizni kiriting'))
+
+# if yosh >= 18:
+#     print('kattasan')
+# elif yosh >= 13:
+#     print('osmir san')
+# else :
+#     print('kichiksan')
+
+# if yosh >= 18 and yosh  <= 60 :
+#     print('ish yoshida')
+# else:
+#     print('siz ishlay olmaysiz')
+
+# kun = input('bugun qaysi kun')
+
+# if kun == 'shanba' or kun == 'yakshanba' :
+#     print('dam olish kuni')
+# else:
+#     print('ish kuni')
+
+# if not yosh < 18:
+#     print ('voyaga yetgansiz')
+
+# son = int(input('son kiriting : '))
+
+# if son > 0 :
+#     print('musbat')
+# elif son < 0:
+#     print('manfiy')
+# else:
+#     print('nol')
+
+# if son % 2 == 0:
+#     print('juft')
+# else:
+#     print('toq')
+
+# ===========================================================
+#   1 - TOPSHIRIQ
+# ===========================================================
+# ball = int(input('son kiriting : '))
+
+# if ball <= 100 and ball >=90 :
+#     print('alo')
+# elif ball <=89 and ball >= 75 :
+#     print('yaxshi')
+# elif ball <= 74 and ball >=60 :
+#     print('qoniqrli')
+# elif ball <= 59 and ball >=0 :
+#     print('qoniqrsiz')
+# else :
+#     print("100 dan 0 gacha son kiriting boshqa emas")
+
+# kyil = int(input('yill kiriting : '))
+# print(kyil)
+# if kyil % 4 ==0 and kyil % 100 == 0:
+#     print(f'{kyil} kabisa yill')
+# else :
+#     print(f'{kyil}kabisa yil emas')
+
+# ===========================================================
+#   2 - TOPSHIRIQ
+# ===========================================================
+
+# tok = int(input('tok klw tini kiriting : '))
+
+# tr1 = 100
+# tr2 = 250
+# tr3 = 300
+# ayr = tr2 - tr1
+# ayr2 = tr2 - ayr 
+# ayr3 = tr3 - ayr2
+# hisob = tr1 *1
+# hisob2 = ayr *1.5
+# hisob3 = ayr3 * 2
+
+# if tok >=0 and tok <=tr1 :
+#     print(f'tok narxi {hisob}')
+# elif tok >= tr1 + 1 and tok <= tr2:
+#     # qosh = hisob1 + hisob
+#     um1 = hisob + hisob2
+#     print(f'tok narxi {um1}')
+# elif tok>= tr2 + 1 and tok <= tr3 :
+#     umumiy = hisob + hisob2 + hisob3
+#     print(f'tok narxi {umumiy}')
+
+# ===========================================================
+#   4- DARS
+# ===========================================================
+

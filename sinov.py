0bb2dbccea0fb7ea0404aaa5117dccf50f5d9af1
Behruz <behruzyunusboyev68@gmail.com>
2026-05-23 18:02:57 +0500

    sinov yarTLDI

---
+++ sinov.py
@@ -0,0 +1,12 @@
+# ism = input("ism yoz : ")
+# yosh = int(input('yosh : '))
+
+# if yosh >= 18 :
+#     print(f'{ism} sen kirishin mumkin')
+# else :
+#     print(f'{ism} sen kirolmaysan {18 - yosh} yildan song kirasan')
+
+# for i in range(0,100 + 1):
+#     if i % 2 == 0:
+#         print(i)
+#     i+=1
